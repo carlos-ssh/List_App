359bba3f492d0292342211c93d71363f04883296
Carlos-SSH <carlos.anezeh@me.com>
2020-08-26 17:01:25 -0500

    Class called Item:
    Created Class Item and added an initialize and assignet a method for checked

--- item.rb
+++ item.rb
@@ -1,2 +1,17 @@
-puts "Hello World"
+class Item
+  attr_writer :text, :checked
 
+  def initialize(text="")
+    @text = text
+    @checked = checked
+  end
+
+  def to_s
+    if @checked
+      "[ X ]" + @text
+    else
+      "[  ]" + @text
+    end
+  end
+
+end
