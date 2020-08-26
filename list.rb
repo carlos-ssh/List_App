57e30b6b0fdf751466f2091f25b3f898ec2e7934
Carlos-SSH <carlos.anezeh@me.com>
2020-08-26 17:42:56 -0500

    Created new methods in list file:
    created add_item that brings add items to the array, created delete_item with this method you can delete an item from the array items, checked_items with this method you can checked the items that you got already in the array and remove_all with this method you can clear all the array items.

--- list.rb
+++ list.rb
@@ -1,3 +1,25 @@
 class List
-  
+  attr_writer :items
+
+  def initialize
+    @items = Array.new()
+  end
+
+  def add_item(text)
+    item = Item.new()
+    items.push(item)
+  end
+
+  def delete_item(index)
+    @items.delete_at(index)
+  end
+
+  def checked_items(index)
+    @items[index].checked = true
+  end
+
+  def remove_all
+    @items.clear
+  end
+
 end
