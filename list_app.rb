dce1090a9af2691bb327a402584e09d7ab99ec29
Carlos-SSH <carlos.anezeh@me.com>
2020-08-26 19:13:28 -0500

    Created a user interface.

--- list_app.rb
+++ list_app.rb
@@ -10,6 +10,22 @@ class ListApp
 
   def run
     puts "Welcome to List_App by Carlos-SSH"
+
+    loop do
+      puts "a - Add New Item"
+      puts "r - Remove Item"
+      puts "s - Show All Items"
+      puts "m - Mark an Item"
+      puts "d - Delete All Items"
+      puts "e - Exit App"
+      input = gets.chomp
+
+      case input
+      when "e"
+        break
+      end
+    end
+    puts "Thanks for use List_App"
   end
 end
 
