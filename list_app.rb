6058c914133e04b945bbf13ef5fad3e6fabc6d2f
Carlos-SSH <carlos.anezeh@me.com>
2020-08-27 13:18:34 -0500

    Linked input getted by user and converted to use command in every function.

--- list_app.rb
+++ list_app.rb
@@ -12,15 +12,42 @@ class ListApp
     puts "Welcome to List_App by Carlos-SSH"
 
     loop do
-      puts "a - Add New Item"
+      puts "a - Add New Item "
       puts "r - Remove Item"
-      puts "s - Show All Items"
+      puts "s - Show All Items "
       puts "m - Mark an Item"
-      puts "d - Delete All Items"
-      puts "e - Exit App"
+      puts "d - Delete All Items "
+      puts "e - Exit App "
       input = gets.chomp
 
       case input
+      when "a"
+        puts "¿What do you want to add?"
+        item = gets.chomp
+        @list.add_item(item)
+        puts "Your Item was added successfully!"
+      when "r"
+        puts "Please put your item that you want remove: "
+        index = gets.chomp
+        @list.delete_item(index.to_i)
+        puts "Item Number: #{index} Deleted"
+      when "s"
+        puts "Shopping List: "
+        @list.show_all
+      when "m"
+        puts "What Item do you want to Mark: "
+        index = gets.chomp
+        @list.check_item(index.to_i)
+        puts "Item Marked"
+      when "d"
+        puts "Do you want remove all your items? y / n"
+        input = gets.chomp
+        if input == "y"
+          @list.remove_all
+          puts "Your list has been removed "
+        else
+          puts "Canceled"
+        end
       when "e"
         break
       end
